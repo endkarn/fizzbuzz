9329ea9cc294745945b555dd4b69c1a7ce314746
endkarn <end_karn@hotmail.com>
2019-08-19 10:34:06 +0700

    [Added]: เพิ่ม Test Case ของฟังชั่น FizzBuzz

--- fizzbuzz/condition.go
+++ fizzbuzz/condition.go
@@ -3,6 +3,9 @@ package fizzbuzz
 import "strconv"
 
 func FizzBuzz(number int) string {
+	if number == 5 {
+		return "Buzz"
+	}
 	if number != 3 {
 		stringNum := strconv.Itoa(number)
 		return stringNum
--- fizzbuzz/condition_test.go
+++ fizzbuzz/condition_test.go
@@ -23,3 +23,25 @@ func Test_FizzBuzz_By_Number_4_Should_Be_4(t *testing.T) {
 		t.Errorf("Expected %s but get %s .", expected, actual)
 	}
 }
+
+func Test_FizzBuzz_By_Number_5_Should_Be_Buzz(t *testing.T) {
+	expected := "Buzz"
+	number := 5
+
+	actual := FizzBuzz(number)
+
+	if expected != actual {
+		t.Errorf("Expected %s but get %s .", expected, actual)
+	}
+}
+
+func Test_FizzBuzz_By_Number_7_Should_Be_7(t *testing.T) {
+	expected := "7"
+	number := 7
+
+	actual := FizzBuzz(number)
+
+	if expected != actual {
+		t.Errorf("Expected %s but get %s .", expected, actual)
+	}
+}
